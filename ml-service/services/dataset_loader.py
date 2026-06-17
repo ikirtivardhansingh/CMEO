3a058adf6e82b6ec552efcd940f774379fae5e42
ikirtvardhansingh <ikirtivardhansingh@gmail.com>
2026-06-17 12:30:33 +0530

    p1s1: Validator wrap migh need changes in future based upon the initial segment of the portion

--- ml-service/services/dataset_loader.py
+++ ml-service/services/dataset_loader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from validator import Validator
 
 class DatasetLoader:
 
@@ -11,3 +12,20 @@ df = loader.load("datasets/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
 print(df.head())
 print(df.columns)
+
+validator = Validator()
+validator.validate_not_empty(df)
+validator.validate_target_column(
+    df,
+    "Churn"
+)
+validator.validate_required_columns(
+    df,
+    [
+        "tenure",
+        "MonthlyCharges",
+        "Contract"
+    ]
+)
+
+print("All validations passed")
--- ml-service/services/validator.py
+++ ml-service/services/validator.py
@@ -1,6 +1,6 @@
 class Validator:
     
-    def validate_taget_columns(self, df, target_column):
+    def validate_target_column(self, df, target_column):
         if target_column not in df.columns:
             raise ValueError(
                 f"Target column '{target_column}' not found in dataset"
@@ -15,3 +15,12 @@ class Validator:
             )
         
         return True
+    def validate_required_columns(self, df, required_columns):
+        
+        for column in required_columns:
+            if column not in df.columns:
+                raise ValueError(
+                    f"Required column '{columns}' not found in the dataset"
+                )
+            
+            return True
