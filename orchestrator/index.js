f8b37e8c149ac2d8f5f6af3637caf9f0203e5077
ikirtvardhansingh <ikirtivardhansingh@gmail.com>
2026-07-01 22:10:53 +0530

    P2S4: Flask communication layer completed

--- orchestrator/index.js
+++ orchestrator/index.js
@@ -1,7 +1,28 @@
 import loadConfig from "./configLoader.js";
 import validateConfig from "./configValidator.js";
+import sendRequest from "./httpService.js";
 const config = await loadConfig();
 
 validateConfig(config);
 console.log("Success");
 console.log(config);
+const payload = {
+    model: "LogisticRegressionModel",
+    features: [
+        24,
+        65.5,
+        1572.0,
+        0,
+        "Yes",
+        "No",
+        "Yes",
+        "Month-to-month"
+    ]
+};
+const BASE_URL = "http://127.0.0.1:5000";
+const result = await sendRequest(
+    `${BASE_URL}/predict`,
+    payload
+);
+
+console.log(result);
