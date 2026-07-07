8917420bae4f42fe10755101b5e5b6e3946c7bad
ikirtvardhansingh <ikirtivardhansingh@gmail.com>
2026-07-07 20:25:42 +0530

    P2S6: Exp Runner 2 wrap.

--- orchestrator/ExperimentRunner.js
+++ orchestrator/ExperimentRunner.js
@@ -1,10 +1,35 @@
-import {v4 as uuidv4 } from "uuid";
+import sendRequest from "./httpService.js";
 
 class ExperimentRunner {
     constructor(config) {
         this.config = config;
-        this.experimentId = uuidv4(); 
+
     }
+    async run() {
+        const results = [];
+        for ( const model of this.config.models) {
+            const payload = {
+                model: model,
+                features: [
+                    24,
+                    65.5,
+                    1572.0,
+                    0,
+                    "Yes",
+                    "No",
+                    "Yes",
+                    "Month-to-month"
+                ]
+            };
+            const BASE_URL = "http://127.0.0.1:5000";
+            const result = 
+                await sendRequest(
+                    `${BASE_URL} /predict`, 
+                    payload);
+            }
+            results.push({model, result});
+            return result;
+        }
 }
 
 export default ExperimentRunner;
--- orchestrator/index.js
+++ orchestrator/index.js
@@ -1,32 +1,11 @@
 import loadConfig from "./configLoader.js";
 import validateConfig from "./configValidator.js";
-import sendRequest from "./httpService.js";
 import ExperimentRunner from "./ExperimentRunner.js";
 
 
 const config = await loadConfig();
-
-
 validateConfig(config);
-console.log("Success");
+const runner = new ExperimentRunner(config);
+const results = await runner.run();
+console.log(results);
 console.log(config);
-const payload = {
-    model: "LogisticRegressionModel",
-    features: [
-        24,
-        65.5,
-        1572.0,
-        0,
-        "Yes",
-        "No",
-        "Yes",
-        "Month-to-month"
-    ]
-};
-const BASE_URL = "http://127.0.0.1:5000";
-const result = await sendRequest(
-    `${BASE_URL}/predict`,
-    payload
-);
-
-console.log(result);
